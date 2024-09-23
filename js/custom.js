51a82dcf4e994b63b08d787e9da7dba2d7a63505
Mahidishikder <mahidishikder@gmail.com>
2024-09-23 16:00:59 +0600

    hello

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,58 @@
 
 // Blog and Home work in manu
-document.getElementById('btn-blog').addEventListener('click',function(){
+document.getElementById('btn-blog').addEventListener('click',function(e){
+  e.preventDefault()
   document.location.href = './blog.html';
 });
 
 
-// button hidden and show
-document.getElementById('btn-history').addEventListener('click',function(){
-  let donation = document.getElementById('btn-donation');
-  let history = document.getElementById('btn-history');
-  donation.style.background = 'white';
-  history.style.background = '#B4F461'
+// // button hidden and show
+// document.getElementById('btn-history').addEventListener('click',function(e){
+//   e.preventDefault()
+//   let donation = document.getElementById('btn-donation');
+//   let history = document.getElementById('btn-history');
+//   donation.style.background = 'white';
+//   history.style.background = '#B4F461';
+
+
+// })
+
+// document.getElementById('btn-donation').addEventListener('click',function(e){
+//   e.preventDefault()
+//   let donation = document.getElementById('btn-donation');
+//   let history = document.getElementById('btn-history');
+//   history.style.background = 'white'
+//   donation.style.background = '#B4F461';
+
+// })
+
+document.getElementById('btn-history').addEventListener('click',function(e){
+  e.preventDefault()
+  document.location.href = './history.html';
+  
 
 })
 
-document.getElementById('btn-donation').addEventListener('click',function(){
-  let donation = document.getElementById('btn-donation');
-  let history = document.getElementById('btn-history');
-  history.style.background = 'white'
-  donation.style.background = '#B4F461';
+document.getElementById('btn-donation').addEventListener('click',function(e){
+  e.preventDefault()
+  document.location.href = './index.html'
+})
 
+document.getElementById('btn-donate-card').addEventListener('click',function(){
+  let input = parseFloat(document.getElementById('input-card').value)
+  let noakhaliDonate = parseFloat(document.getElementById('noakhali-donate').innerText)
+  let myAmount = parseFloat(document.getElementById('my-amount').innerText)
+  let add = noakhaliDonate + input;
+  let myAccountDecreamente = myAmount - input
+  console.log(myAmount,noakhaliDonate)
+
+  document.getElementById('my-amount').innerText = myAccountDecreamente
+  document.getElementById('noakhali-donate').innerText = add;
+  document.getElementById('input-card').value = ''
 
 })
+
+
 
 
 
